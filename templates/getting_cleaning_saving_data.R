3450652242de5a27c4f34ee561d282cbfd8d7c67
twixer <twixer@gmail.com>
2014-09-21 16:42:46 +0200

    feat(templates) add a new template getting_cleaning_saving_data.R

---
+++ templates/getting_cleaning_saving_data.R
@@ -0,0 +1,20 @@
+# =============================================================================
+#
+# This script load raw data, manipulate them and finally save them in a R data 
+# structure.
+#
+# =============================================================================
+
+# ================
+# Getting and saving raw data
+# ================
+
+
+# ================
+# Cleaning raw data and saving them
+# ================
+
+
+# ================
+# Loading data
+# ================

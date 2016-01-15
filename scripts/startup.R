b2a30eee09b1b6323c276293ad9c5233c827a416
twixer <twixer@gmail.com>
2016-01-16 00:23:22 +0100

    feat(script) adapt the startup scritp to R v3.2.3

--- scripts/startup.R
+++ scripts/startup.R
@@ -1,8 +1,7 @@
 # =============================================================================
 #
 # A startup script for a new R installation. 
-# This script contains install command for a 
-# list of usefull R packages.
+# This script contains install command for a list of usefull R packages.
 # 
 # Convention : 
 #     - comments : a information link explaining the package (not the doc)
@@ -13,47 +12,57 @@
 
 # http://www.r-statistics.com/2013/06/top-100-r-packages-for-2013-jan-may/
 if (!require('devtools', quietly = F))  install.packages("devtools") 
+# package ‘magrittr’ successfully unpacked and MD5 sums checked
+# package ‘BH’ successfully unpacked and MD5 sums checked
+# package ‘mime’ successfully unpacked and MD5 sums checked
+# package ‘R6’ successfully unpacked and MD5 sums checked
 # package ‘stringr’ successfully unpacked and MD5 sums checked
-# package ‘jsonlite’ successfully unpacked and MD5 sums checked
-# package ‘bitops’ successfully unpacked and MD5 sums checked
+# package ‘stringi’ successfully unpacked and MD5 sums checked
+# package ‘brew’ successfully unpacked and MD5 sums checked
+# package ‘Rcpp’ successfully unpacked and MD5 sums checked
+# package ‘xml2’ successfully unpacked and MD5 sums checked
 # package ‘httr’ successfully unpacked and MD5 sums checked
-# package ‘RCurl’ successfully unpacked and MD5 sums checked
+# package ‘curl’ successfully unpacked and MD5 sums checked
 # package ‘memoise’ successfully unpacked and MD5 sums checked
 # package ‘whisker’ successfully unpacked and MD5 sums checked
 # package ‘evaluate’ successfully unpacked and MD5 sums checked
 # package ‘digest’ successfully unpacked and MD5 sums checked
+# package ‘rstudioapi’ successfully unpacked and MD5 sums checked
+# package ‘jsonlite’ successfully unpacked and MD5 sums checked
+# package ‘roxygen2’ successfully unpacked and MD5 sums checked
+# package ‘rversions’ successfully unpacked and MD5 sums checked
+# package ‘git2r’ successfully unpacked and MD5 sums checked
 # package ‘devtools’ successfully unpacked and MD5 sums checked
 
 #https://github.com/trinker/pacman/
-install_github("pacman", "trinker")
+#install_github("trinker/pacman")
+if (!require('pacman', quietly = F))  install.packages("pacman")
 
 # http://www.r-statistics.com/2014/08/simpler-r-coding-with-pipes-the-present-and-future-of-the-magrittr-package/
-if (!require('magrittr', quietly = F)) install.packages("magrittr") 
+#if (!require('magrittr', quietly = F)) install.packages("magrittr") 
 
 if (!require('Amelia', quietly = F)) install.packages("Amelia") 
-# package ‘Rcpp’ successfully unpacked and MD5 sums checked
 # package ‘RcppArmadillo’ successfully unpacked and MD5 sums checked
 # package ‘Amelia’ successfully unpacked and MD5 sums checked
 
 if (!require('dplyr', quietly = F)) install.packages("dplyr")
 # package ‘assertthat’ successfully unpacked and MD5 sums checked
-# package ‘Lahman’ successfully unpacked and MD5 sums checked
-# package ‘hflights’ successfully unpacked and MD5 sums checked
-# package ‘BH’ successfully unpacked and MD5 sums checked
+# package ‘lazyeval’ successfully unpacked and MD5 sums checked
+# package ‘DBI’ successfully unpacked and MD5 sums checked
 # package ‘dplyr’ successfully unpacked and MD5 sums checked
 
 if (!require('rJava', quietly = F)) install.packages("rJava") 
 
 if (!require('ggplot2', quietly = F)) install.packages("ggplot2") 
+# package ‘colorspace’ successfully unpacked and MD5 sums checked
 # package ‘RColorBrewer’ successfully unpacked and MD5 sums checked
 # package ‘dichromat’ successfully unpacked and MD5 sums checked
 # package ‘munsell’ successfully unpacked and MD5 sums checked
 # package ‘labeling’ successfully unpacked and MD5 sums checked
-# package ‘plyr’ successfully unpacked and MD5 sums checked
 # package ‘gtable’ successfully unpacked and MD5 sums checked
+# package ‘plyr’ successfully unpacked and MD5 sums checked
 # package ‘reshape2’ successfully unpacked and MD5 sums checked
 # package ‘scales’ successfully unpacked and MD5 sums checked
-# package ‘proto’ successfully unpacked and MD5 sums checked
 # package ‘ggplot2’ successfully unpacked and MD5 sums checked
 
 if (!require('xtable', quietly = F)) install.packages("xtable")
@@ -84,47 +93,54 @@ if (!require('installr', quietly = F)) install.packages("installr")
 # http://www.inside-r.org/packages/cran/log4r/docs/log4r
 if (!require('log4r', quietly = F)) install.packages("log4r")
 
+# Text Mining, Dialog Analysis
 # http://trinker.github.io/qdap/vignettes/qdap_vignette.html
-install_github("qdap", "trinker")
-# package ‘RJSONIO’ successfully unpacked and MD5 sums checked
-# package ‘bibtex’ successfully unpacked and MD5 sums checked
-# package ‘lubridate’ successfully unpacked and MD5 sums checked
-# package ‘RefManageR’ successfully unpacked and MD5 sums checked
+if (!require('qdap', quietly = F)) install.packages("qdap")
+# package ‘iterators’ successfully unpacked and MD5 sums checked
+# package ‘pkgmaker’ successfully unpacked and MD5 sums checked
+# package ‘registry’ successfully unpacked and MD5 sums checked
+# package ‘rngtools’ successfully unpacked and MD5 sums checked
+# package ‘gridBase’ successfully unpacked and MD5 sums checked
+# package ‘foreach’ successfully unpacked and MD5 sums checked
+# package ‘doParallel’ successfully unpacked and MD5 sums checked
+# package ‘data.table’ successfully unpacked and MD5 sums checked
+# package ‘gtools’ successfully unpacked and MD5 sums checked
+# package ‘NMF’ successfully unpacked and MD5 sums checked
+# package ‘irlba’ successfully unpacked and MD5 sums checked
 # package ‘openNLPdata’ successfully unpacked and MD5 sums checked
-# package ‘knitcitations’ successfully unpacked and MD5 sums checked
+# package ‘bitops’ successfully unpacked and MD5 sums checked
 # package ‘slam’ successfully unpacked and MD5 sums checked
 # package ‘qdapDictionaries’ successfully unpacked and MD5 sums checked
 # package ‘qdapRegex’ successfully unpacked and MD5 sums checked
 # package ‘qdapTools’ successfully unpacked and MD5 sums checked
 # package ‘chron’ successfully unpacked and MD5 sums checked
+# package ‘gdata’ successfully unpacked and MD5 sums checked
+# package ‘gender’ successfully unpacked and MD5 sums checked
 # package ‘gridExtra’ successfully unpacked and MD5 sums checked
 # package ‘igraph’ successfully unpacked and MD5 sums checked
 # package ‘NLP’ successfully unpacked and MD5 sums checked
 # package ‘openNLP’ successfully unpacked and MD5 sums checked
 # package ‘plotrix’ successfully unpacked and MD5 sums checked
+# package ‘RCurl’ successfully unpacked and MD5 sums checked
 # package ‘reports’ successfully unpacked and MD5 sums checked
 # package ‘stringdist’ successfully unpacked and MD5 sums checked
+# package ‘tidyr’ successfully unpacked and MD5 sums checked
 # package ‘tm’ successfully unpacked and MD5 sums checked
 # package ‘venneuler’ successfully unpacked and MD5 sums checked
 # package ‘wordcloud’ successfully unpacked and MD5 sums checked
-# package ‘koRpus’ successfully unpacked and MD5 sums checked
-# package ‘lda’ successfully unpacked and MD5 sums checked
-# package ‘proxy’ successfully unpacked and MD5 sums checked
-# package ‘stringi’ successfully unpacked and MD5 sums checked
-# package ‘SnowballC’ successfully unpacked and MD5 sums checked
-# package ‘testthat’ successfully unpacked and MD5 sums checked
+# package ‘XML’ successfully unpacked and MD5 sums checked
+# package ‘qdap’ successfully unpacked and MD5 sums checked
 
-# http://www.r-bloggers.com/how-to-publish-r-and-ggplot2-to-the-web/
-install_github("ropensci/plotly")
+# https://plot.ly/r/
+if (!require('plotly', quietly = F)) install.packages("plotly")
+# package ‘htmltools’ successfully unpacked and MD5 sums checked
+# package ‘yaml’ successfully unpacked and MD5 sums checked
+# package ‘viridis’ successfully unpacked and MD5 sums checked
+# package ‘base64enc’ successfully unpacked and MD5 sums checked
+# package ‘htmlwidgets’ successfully unpacked and MD5 sums checked
+# package ‘plotly’ successfully unpacked and MD5 sums checked
 
 # http://www.r-bloggers.com/seeing-the-daylight-with-r/
 if (!require('maptools', quietly = F)) install.packages("maptools")
 
 if (!require('fortunes', quietly = F)) install.packages("fortunes")
-
-# Text Minning
-if (!require('tm', quietly = F)) install.packages("tm")
-if (!require('openNLP', quietly = F)) install.packages("openNLP")
-
-
-
